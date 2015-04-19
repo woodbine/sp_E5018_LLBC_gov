cbda160c15841b9fff1f6d5fce8bba1b7ac807da
woodbine <ian@spendnetwork.com>
2015-04-19 17:16:49 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -30,11 +30,11 @@ pageLinks = menuBlock.findAll('a', href=True)
 
 for pageLink in pageLinks:
 	href = pageLink['href'].encode('utf8')
-	href = "http://www.lewisham.gov.uk/"+href
 	parsed_link = urlparse.urlsplit(href.encode('utf8'))
 	parsed_link = parsed_link._replace(path=urllib.quote(parsed_link.path))
 	encoded_link = parsed_link.geturl()
-  	html2 = urllib2.urlopen(encoded_link)
+	fullLink = "http://www.lewisham.gov.uk/"+encoded_link
+  	html2 = urllib2.urlopen(fullLink)
   	soup2 = BeautifulSoup(html2)
   	
   	docBlock = soup2.find('div',{'class':'contentContainer documentsList'})
