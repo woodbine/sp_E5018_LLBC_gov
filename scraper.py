653d9df0dcb426f947c726d719bfa64dd76462f6
woodbine <ian@spendnetwork.com>
2015-04-19 12:22:41 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@ menuBlock = soup.find('td',{'class':'lbl-styleTableEvenCol-inThisSectionBox'}) #
 pageLinks = menuBlock.findAll('a', href=True)
 
 for pageLink in pageLinks:
-	href = pageLink['href']
+	href = pageLink['href'].encode('utf8')
 	href = "http://www.lewisham.gov.uk/"+href
   	html2 = urllib2.urlopen(href)
   	soup2 = BeautifulSoup(html2)
